4804bd75793b1be6b75003bd3a7cd6ba18385745
AndrewYuZhenYu <Zhenyu_yu@outlook.com>
2026-03-19 21:46:57 +0800

    20260319.3

--- cppc_codes_first_learning/hello_ github.cpp
+++ cppc_codes_first_learning/hello_ github.cpp
@@ -3,6 +3,7 @@ int main()
 {
     using namespace std;
     cout << "Hello github " << endl
-         << "hello future" << endl;
+         << "hello future" << endl
+         << "孟凡一牛逼！";
     return 0; // 现在大功告成了！
 }
