d5efc9797a389f40f423f18e0d110b7ef3e10467
AndrewYuZhenYu <Zhenyu_yu@outlook.com>
2026-03-22 20:25:10 +0800

    20260322.3

--- c++_codes_first_learning/2026032201.cpp
+++ c++_codes_first_learning/2026032201.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 double square(double x)
 {
     return x * x;
@@ -10,5 +11,7 @@ int main()
     auto test_1 = 3.1415926;
     // cout << "the number of test_1 is " << test_1 << "\n";
     cin >> x;
-    cout << "The number of x is " << x;
+    cout << "The number of x is " << x << endl;
+    cout << "x的三次方是" << pow(x, 3) << "\n";
+    cout << "x的五次方是" << pow(x, 5) << endl;
 }
